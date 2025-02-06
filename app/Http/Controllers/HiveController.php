d5b2fd8696013a3b1c19cae5f4ff4aadb8d601bc
Denis40-prog <denis.chevanne@hotmail.fr>
2025-02-06 12:05:12 +0100

    [ADD] #8 - get hive & apiary + [UPD] déplacement des routes d'auth

--- app/Http/Controllers/HiveController.php
+++ app/Http/Controllers/HiveController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Hive;
+use Illuminate\Http\JsonResponse;
 
 class HiveController extends Controller
 {
-    //
+    public function about(int $idHive): JsonResponse
+    {
+        $hive = Hive::findOrFail($idHive);
+
+        $apiary = $hive->apiary()->select(['id','name'])->firstOrFail();
+
+        return response()->json([
+            'hive' => $hive,
+            'apiary' => $apiary
+        ]);
+    }
+
+    public function isSick(int $idHive) : JsonResponse
+    {
+        $hive = Hive::findOrFail($idHive);
+
+        $isSick = $hive->diseases()->where('hive_id', $idHive)->whereNull('dateEnd')->exists();
+
+        return response()->json([
+            'isSick' => $isSick
+        ]);
+    }
+
+    public function wasSick(int $idHive) : JsonResponse
+    {
+        $hive = Hive::findOrFail($idHive);
+
+        $wasSick = $hive->diseases()->where('hive_id', $idHive)->whereNotNull('dateEnd')->exists();
+
+        return response()->json([
+            'wasSick' => $wasSick
+        ]);
+    }
 }
